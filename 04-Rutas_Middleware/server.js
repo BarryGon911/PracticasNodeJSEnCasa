0931d1d95d073398b0895b4d18ff5f1e947dfc97
BarryGon911 <corragio.sage@gmail.com>
2025-06-17 19:55:01 -0600

    Cambios en clase presencial
    Cambios de la primera hora

--- 04-Rutas_Middleware/server.js
+++ 04-Rutas_Middleware/server.js
@@ -1,21 +1,34 @@
 const http = require("http");
+const PORT = 3000;
 
 function logEvents(req, resp, next) {
-  const hora = new Date().toLocaleTimeString();
-  console.log(`${hora} Alguien vino a ${req.url}`);
+  const dateTime = new Date();
+  const fecha = dateTime.localTimeString();
+  const tiempo = dateTime.localTimeString();
+  console.log(`${fecha}-${tiempo} | Solicitud a: ${req.url}`);
   next();
+}
+
+// http://localhost:3000/?name=Barrygon911
+function validarNombre(req, res, next) {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const nombre = url.searchParams.get("name");
+  if (!nombre) {
+    res.statusCode = 400;
+    return res.end("Falta el parámetro de la URL");
+  }
 }
 
 const server = http.createServer((req, res) => {
   logEvents(req, res, () => {
-    if (req.url === "/biblioteca") {
-      res.end("Bienvenido a la Biblioteca");
+    if (req.url === "/") {
+      res.end("Welcome Gallegos");
     } else {
-      res.end("¡Ups! Lo siento, no conozco esa ruta");
+      res.end("404");
     }
   });
 });
 
-server.listen(3000, () => {
-  console.log("NodeJS Server is running on Port 3000");
+server.listen(PORT, () => {
+  console.log(`NodeJS Server is running on Port ${PORT}`);
 });

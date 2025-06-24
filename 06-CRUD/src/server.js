112ff1418a4de543c6040f3678ef5449028cf97d
BarryGon911 <corragio.sage@gmail.com>
2025-06-23 19:32:37 -0600

    Edited server.js

--- 06-CRUD/src/server.js
+++ 06-CRUD/src/server.js
@@ -5,22 +5,6 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to ExpressJS");
-});
-
-app.get("/memes", (req, res) => {
-  res.json({
-    image: "05-ExpressJS/src/DoggyFace.png",
-    description: "When you realize your Doggie face on Mondays",
-  });
-});
-
-app.get("/libros/:id", (req, res) => {
-  const libroId = req.params.id;// Accessing the ID from the URL parameters
-  res.send(`Book ID: ${libroId} succesfully located`);
-});
-
 app.listen(port, () => {
   console.log(
     colors.bgMagenta.magenta.italic.bold(

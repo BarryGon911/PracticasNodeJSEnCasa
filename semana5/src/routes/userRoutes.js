248044602e7b599c1bdc3c7b71daeeb22c8a14a2
BarryGon911 <corragio.sage@gmail.com>
2025-06-25 12:08:51 -0600

    Changes

--- semana5/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { logger } from '../middlewares/logger.js';
-import { addUser, getUsers } from '../controllers/userController.js';
-
-const router = express.Router();
-router.get('/users', logger, getUsers);
-router.post('/users', logger, addUser);
-
-export default router;
--- semana5/src/server.js
+++ semana5/src/server.js
@@ -2,22 +2,31 @@ import express from "express";
 import colors from "colors";
 import "dotenv/config";
 import { logger } from "../src/middlewares/logger.js";
-import usersRouter from "../src/routes/userRoutes.js";
+
+//Importamos la función loadData para cargar los datos al iniciar el servidor y el router de usuarios para manejar las rutas relacionadas con los usuarios.
+import { loadData } from "../src/storage.js";
+import usersRouter from "../src/routes/usersRoutes.js";
+
+const port = process.env.PORT || 3000;
+await loadData();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(express.json());// Espress debe ser capaz de recibir datos en formato JSON
+// Express debe ser capaz de recibir datos en formato JSON
+app.use(express.json());/
 
-app.get("/:name", logger, (req, res) => {
-  //http://localhost:3000/yeriko?isAdmin=true
-  if(req.query.isAdmin === "true") {
+app.get('/:name', logger, (req, res) => {
+  //http://localhost:3000/BarryGon911?isAdmin=true
+  if (req.query.isAdmin === 'true') {
     res.end(`Welcome Admin ${req.params.name} to your API`);
-  } else {
-    es.end(`Welcome ${req.params.name}`);
   }
-  res.send("Hello, Welcome to Your ExpressJS Server");
+  else {
+    res.end(`Welcome ${req.params.name}`);
+  }
+  res.end('Hola');
 });
+
+app.use('/api', usersRouter);
 
 app.listen(port, () => {
   console.log(colors.bgMagenta.magenta.italic.bold(`ExpressJS Server is running on http://localhost:${port}`));

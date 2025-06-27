11322a1cffb4ad9a9c9e7cfe46608e3aa5dc40b3
BarryGon911 <corragio.sage@gmail.com>
2025-06-27 10:41:07 -0600

    Changes

---
+++ semana5/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import colors from "colors";
+import "dotenv/config";
+import express from 'express';
+import { logger } from './src/middlewares/logger.js';
+import { loadData } from './src/storage.js';
+import usersRouter from './src/routes/usersRoutes.js';
+
+const PORT = 3000;
+await loadData();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/:name', logger, (req, res) => {
+  //http://localhost:3000/Rodrigo?isAdmin=true
+  if (req.query.isAdmin === 'true') {
+    res.end(`Welcome Admin ${req.params.name} to your API`);
+  }
+  else {
+    res.end(`Welcome ${req.params.name}`);
+  }
+  res.end('Hola');
+});
+
+app.use('/api', usersRouter);
+//app.use('/api', prodctsRouter);
+
+app.get('/salud/:name', logger, () => {
+  const { name } = req.params;
+    res.json({mensaje: `Hola ${name}` })
+  
+});
+
+app.listen(port, () => {
+  console.log(
+    colors.bgMagenta.magenta.italic.bold(
+      `ExpressJS Server is running on http://localhost:${port}`
+    )
+  );
+});
